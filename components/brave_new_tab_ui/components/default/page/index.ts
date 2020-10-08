45c884c9763e9c2dd050ea187fddd3d2a1eb5049
James Tedy <james.tedy@ymail.com>
2020-10-08 18:39:29 +1100

    Fix CSS so top sites don't move when clock toggled on/off
    Fix #11484
    
    Change the CSS of:
    - GridItemWidgetStack
    - GridItemTopSites
    - GridItemNavigation
    - GridItemCredits
    
    getItemRowCount will always give 3 (--ntp-item-row-count: 3)

--- components/brave_new_tab_ui/components/default/page/index.ts
+++ components/brave_new_tab_ui/components/default/page/index.ts
@@ -2,44 +2,44 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import styled, { css } from 'brave-ui/theme'
+import styled, { css } from "brave-ui/theme";
 
-const breakpointLargeBlocks = '980px'
-const breakpointEveryBlock = '870px'
+const breakpointLargeBlocks = "980px";
+const breakpointEveryBlock = "870px";
 
 const singleColumnSmallViewport = css`
- @media screen and (max-width: ${breakpointEveryBlock}) {
-   text-align: center;
- }
-`
+  @media screen and (max-width: ${breakpointEveryBlock}) {
+    text-align: center;
+  }
+`;
 
 interface PageProps {
-  showClock: boolean
-  showStats: boolean
-  showRewards: boolean
-  showBinance: boolean
-  showTogether: boolean
-  showTopSites: boolean
-  showAddCard: boolean
-  showBrandedWallpaper: boolean
+  showClock: boolean;
+  showStats: boolean;
+  showRewards: boolean;
+  showBinance: boolean;
+  showTogether: boolean;
+  showTopSites: boolean;
+  showAddCard: boolean;
+  showBrandedWallpaper: boolean;
 }
 
-function getItemRowCount (p: PageProps): number {
-  let right = (p.showClock ? 1 : 0) + (p.showRewards ? 2 : 0)
-  let left = (p.showStats ? 1 : 0) + (p.showTopSites ? 1 : 0)
+function getItemRowCount(p: PageProps): number {
+  let right = (p.showClock ? 1 : 0) + (p.showRewards ? 1 : 0);
+  let left = (p.showStats ? 1 : 0) + (p.showTopSites ? 1 : 0);
   // Has space for branded logo to sit next to something on right?
-  if (p.showBrandedWallpaper && left >= right) {
-    left++
+  if (0 && left >= right) {
+    left++;
   }
-  return Math.max(left, right) + 1 // extra 1 for footer
+  return Math.max(2, left, right) + 1; // extra 1 for footer
 }
 
-export const Page = styled<PageProps, 'div'>('div')`
+export const Page = styled<PageProps, "div">("div")`
   /* Increase the explicit row count when adding new widgets
      so that the footer goes in the correct location always,
      yet can still merge upwards to previous rows. */
   --ntp-item-row-count: ${getItemRowCount};
-  --ntp-extra-footer-rows: ${p => p.showBrandedWallpaper ? 1 : 0};
+  --ntp-extra-footer-rows: ${(p) => (p.showBrandedWallpaper ? 1 : 0)};
   --ntp-space-rows: 0;
   --ntp-page-rows: calc(var(--ntp-item-row-count) + var(--ntp-space-rows));
   --ntp-item-justify: start;
@@ -79,83 +79,88 @@ export const Page = styled<PageProps, 'div'>('div')`
     flex-direction: column;
     align-items: center;
   }
-`
+`;
 
-export const GridItemStats = styled('section')`
+export const GridItemStats = styled("section")`
   grid-column: 1 / span 2;
   ${singleColumnSmallViewport}
-`
+`;
 
-export const GridItemClock = styled('section')`
+export const GridItemClock = styled("section")`
   grid-column: 3;
   justify-self: center;
   ${singleColumnSmallViewport}
-`
+`;
 
-export const GridItemWidgetStack = styled('section')`
+export const GridItemWidgetStack = styled("section")`
   grid-column: 3 / span 1;
-  grid-row-end: span 2;
+  grid-row-end: 3;
   @media screen and (max-width: ${breakpointLargeBlocks}) {
     max-width: 284px;
   }
-`
+`;
 
-export const GridItemTopSites = styled('section')`
+export const GridItemTopSites = styled("section")`
   grid-column: 1 / span 2;
+  grid-row: 2 / span 1;
   ${singleColumnSmallViewport}
-`
+`;
 
-export const GridItemNotification = styled('section')`
+export const GridItemNotification = styled("section")`
   position: fixed;
   left: 50%;
   top: 0;
   transform: translateX(-50%);
-`
+`;
 
-export const GridItemCredits = styled('section')`
+export const GridItemCredits = styled("section")`
   /* Variables for easy inherited override without splitting css rules definition */
   --ntp-grid-item-credits-bottom-margin-wide: 36px;
   --ntp-grid-item-credits-left-margin-narrow: 10px;
-  --ntp-grid-item-credits-left-margin-wide: var(--ntp-grid-item-credits-bottom-margin-wide);
-  grid-column: 1 / span 1;
-  grid-row: calc(-2 - var(--ntp-extra-footer-rows)) / span calc(1 + var(--ntp-extra-footer-rows));
+  --ntp-grid-item-credits-left-margin-wide: var(
+    --ntp-grid-item-credits-bottom-margin-wide
+  );
+  grid-column: 1 / 1;
+  grid-row: calc(-2 - var(--ntp-extra-footer-rows)) / span
+    calc(1 + var(--ntp-extra-footer-rows));
   align-self: end;
 
-  margin: 0 0 var(--ntp-grid-item-credits-bottom-margin-wide) var(--ntp-grid-item-credits-left-margin-wide);
+  margin: 0 0 var(--ntp-grid-item-credits-bottom-margin-wide)
+    var(--ntp-grid-item-credits-left-margin-wide);
   @media screen and (max-width: ${breakpointEveryBlock}) {
     /* Display on left, keeping Navigation on right even on wrapped row. */
     margin: 0 auto 0 var(--ntp-grid-item-credits-left-margin-narrow);
     align-self: unset;
   }
-`
+`;
 
 export const GridItemBrandedLogo = styled(GridItemCredits)`
   --ntp-grid-item-credits-left-margin-narrow: 0;
   --ntp-grid-item-credits-bottom-margin-wide: -8px;
   --ntp-grid-item-credits-left-margin-wide: 22px;
-`
+`;
 
-export const GridItemNavigation = styled('section')`
-  grid-column: 3 / span 1;
-  grid-row: -2 / span 1;
+export const GridItemNavigation = styled("section")`
+  grid-column: 3 / -1;
+  grid-row: 3 / -1;
   align-self: end;
   margin: 0 24px 24px 0;
   @media screen and (max-width: ${breakpointEveryBlock}) {
     margin: 0;
     align-self: flex-end;
   }
-`
+`;
 
-export const GridItemNavigationBraveToday = styled<{}, 'span'>('span')`
+export const GridItemNavigationBraveToday = styled<{}, "span">("span")`
   position: absolute;
   bottom: 60px;
   text-align: center;
   width: 100%;
   color: white;
   font-size: 24px;
-`
+`;
 
-export const Footer = styled<{}, 'footer'>('footer')`
+export const Footer = styled<{}, "footer">("footer")`
   /* Child items are primary Grid items and can slot in to free spaces,
      so this element doesn't do anything on wider viewport widths. */
   display: contents;
@@ -168,9 +173,9 @@ export const Footer = styled<{}, 'footer'>('footer')`
     flex-direction: row;
     align-items: flex-end;
   }
-`
+`;
 
-export const FooterContent = styled('div')`
+export const FooterContent = styled("div")`
   display: contents;
 
   @media screen and (max-width: ${breakpointEveryBlock}) {
@@ -181,37 +186,41 @@ export const FooterContent = styled('div')`
     align-items: center;
     flex-wrap: wrap;
   }
-`
+`;
 
 interface ImageLoadProps {
-  imageHasLoaded: boolean
+  imageHasLoaded: boolean;
 }
 
 interface HasImageProps {
-  hasImage: boolean
+  hasImage: boolean;
 }
 
 interface AppProps {
-  dataIsReady: boolean
+  dataIsReady: boolean;
 }
 
-export const App = styled<AppProps, 'div'>('div')`
+export const App = styled<AppProps, "div">("div")`
   box-sizing: border-box;
   display: flex;
   flex: 1;
   flex-direction: column;
-  transition: opacity .125s ease-out;
-  opacity: ${p => p.dataIsReady ? 1 : 0};
-`
+  transition: opacity 0.125s ease-out;
+  opacity: ${(p) => (p.dataIsReady ? 1 : 0)};
+`;
 
-export const PosterBackground = styled<ImageLoadProps & HasImageProps, 'div'>('div')`
+export const PosterBackground = styled<ImageLoadProps & HasImageProps, "div">(
+  "div"
+)`
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   z-index: 1;
-  ${p => !p.hasImage && `
+  ${(p) =>
+    !p.hasImage &&
+    `
   background: linear-gradient(
         to bottom right,
         #4D54D1,
@@ -222,12 +231,12 @@ export const PosterBackground = styled<ImageLoadProps & HasImageProps, 'div'>('d
     height: 100%;
     object-fit: cover;
     display: block;
-    opacity: ${p => p.imageHasLoaded ? 1 : 0};
-    transition: opacity .5s ease-in-out;
+    opacity: ${(p) => (p.imageHasLoaded ? 1 : 0)};
+    transition: opacity 0.5s ease-in-out;
   }
-`
+`;
 
-export const Gradient = styled<ImageLoadProps, 'div'>('div')`
+export const Gradient = styled<ImageLoadProps, "div">("div")`
   --gradient-bg: linear-gradient(
     rgba(0, 0, 0, 0.8),
     rgba(0, 0, 0, 0) 35%,
@@ -244,13 +253,13 @@ export const Gradient = styled<ImageLoadProps, 'div'>('div')`
       unless the image has loaded.
     This prevents a flash of slightly-darker gradient */
   @media (prefers-color-scheme: dark) {
-    transition: opacity .4s ease-in-out;
-    opacity: ${p => p.imageHasLoaded ? 1 : 0};
+    transition: opacity 0.4s ease-in-out;
+    opacity: ${(p) => (p.imageHasLoaded ? 1 : 0)};
   }
   height: 100vh;
-`
+`;
 
-export const Link = styled<{}, 'a'>('a')`
+export const Link = styled<{}, "a">("a")`
   text-decoration: none;
   transition: color 0.15s ease, filter 0.15s ease;
   color: rgba(255, 255, 255, 0.8);
@@ -258,9 +267,9 @@ export const Link = styled<{}, 'a'>('a')`
   &:hover {
     color: rgba(255, 255, 255, 1);
   }
-`
+`;
 
-export const PhotoName = styled<{}, 'div'>('div')`
+export const PhotoName = styled<{}, "div">("div")`
   align-self: flex-end;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
@@ -268,22 +277,22 @@ export const PhotoName = styled<{}, 'div'>('div')`
   font-family: Muli, sans-serif;
   color: rgba(255, 255, 255, 0.6);
   white-space: nowrap;
-`
+`;
 
-export const Navigation = styled<{}, 'nav'>('nav')`
+export const Navigation = styled<{}, "nav">("nav")`
   align-self: flex-end;
   display: flex;
   justify-content: flex-end;
   height: 40px;
   align-items: center;
-`
+`;
 
 interface IconButtonProps {
-  clickDisabled?: boolean
-  isClickMenu?: boolean
+  clickDisabled?: boolean;
+  isClickMenu?: boolean;
 }
 
-export const IconLink = styled<{}, 'a'>('a')`
+export const IconLink = styled<{}, "a">("a")`
   display: block;
   width: 24px;
   height: 24px;
@@ -296,16 +305,16 @@ export const IconLink = styled<{}, 'a'>('a')`
   &:hover {
     opacity: 0.95;
   }
-`
+`;
 
-export const IconButton = styled<IconButtonProps, 'button'>('button')`
-  pointer-events: ${p => p.clickDisabled && 'none'};
+export const IconButton = styled<IconButtonProps, "button">("button")`
+  pointer-events: ${(p) => p.clickDisabled && "none"};
   display: flex;
   width: 24px;
   height: 24px;
   padding: 0;
   border: none;
-  margin: ${p => p.isClickMenu ? '7' : '0 12'}px;
+  margin: ${(p) => (p.isClickMenu ? "7" : "0 12")}px;
   cursor: pointer;
   color: #ffffff;
   background-color: transparent;
@@ -314,18 +323,20 @@ export const IconButton = styled<IconButtonProps, 'button'>('button')`
   &:hover {
     opacity: 0.95;
   }
-`
+`;
 
 interface IconButtonSideTextProps {
-  textDirection: string
+  textDirection: string;
 }
 
-export const IconButtonSideText = styled<IconButtonSideTextProps, 'label'>('label')`
+export const IconButtonSideText = styled<IconButtonSideTextProps, "label">(
+  "label"
+)`
   display: grid;
   grid-template-columns: auto auto;
   align-items: center;
-  margin-right: ${p => p.textDirection === 'ltr' && '24px'};
-  margin-left: ${p => p.textDirection === 'rtl' && '24px'};
+  margin-right: ${(p) => p.textDirection === "ltr" && "24px"};
+  margin-left: ${(p) => p.textDirection === "rtl" && "24px"};
   color: inherit;
   cursor: pointer;
   user-select: none;
@@ -339,28 +350,32 @@ export const IconButtonSideText = styled<IconButtonSideTextProps, 'label'>('labe
   }
 
   > ${IconButton} {
-    margin-left: ${p => p.textDirection === 'ltr' && '0'};
-    margin-right: ${p => p.textDirection === 'rtl' && '0'};
+    margin-left: ${(p) => p.textDirection === "ltr" && "0"};
+    margin-right: ${(p) => p.textDirection === "rtl" && "0"};
     /* No need to show the outline since the parent is handling it */
     outline: 0;
   }
-`
+`;
 
 interface IconButtonContainerProps {
-  textDirection: string
+  textDirection: string;
 }
 
-export const IconButtonContainer = styled<IconButtonContainerProps, 'div'>('div')`
-  font-family: ${p => p.theme.fontFamily.heading};
+export const IconButtonContainer = styled<IconButtonContainerProps, "div">(
+  "div"
+)`
+  font-family: ${(p) => p.theme.fontFamily.heading};
   font-size: 13px;
   font-weight: 600;
-  color: rgba(255,255,255,0.8);
-  margin-right: ${p => p.textDirection === 'ltr' && '8px'};
-  margin-left: ${p => p.textDirection === 'rtl' && '8px'};
-  border-right: ${p => p.textDirection === 'ltr' && '1px solid rgba(255, 255, 255, 0.6)'};
-  border-left: ${p => p.textDirection === 'rtl' && '1px solid rgba(255, 255, 255, 0.6)'};
+  color: rgba(255, 255, 255, 0.8);
+  margin-right: ${(p) => p.textDirection === "ltr" && "8px"};
+  margin-left: ${(p) => p.textDirection === "rtl" && "8px"};
+  border-right: ${(p) =>
+    p.textDirection === "ltr" && "1px solid rgba(255, 255, 255, 0.6)"};
+  border-left: ${(p) =>
+    p.textDirection === "rtl" && "1px solid rgba(255, 255, 255, 0.6)"};
 
   &:hover {
     color: #ffffff;
   }
-`
+`;
